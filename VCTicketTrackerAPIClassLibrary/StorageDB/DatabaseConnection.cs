e9af4e10eb585a8dee3694e7c360a850aa3cfe42
Jedirednax <166110876+Jedirednax@users.noreply.github.com>
2025-01-19 23:46:09 +0200

    Update DatabaseConnection.cs

--- VCTicketTrackerAPIClassLibrary/StorageDB/DatabaseConnection.cs
+++ VCTicketTrackerAPIClassLibrary/StorageDB/DatabaseConnection.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public class DatabaseConnection
     {
-        //propstatic readonly string conneStr  = @"Server=tcp:vctickettrackerwebapidbserver.database.windows.net,1433;Initial Catalog=vctickettracker;Persist Security Info=False;User ID=ST10021906;password=Varsity@8;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        
         /// <summary>
         /// Store of the active connection string for the database
         /// </summary>
